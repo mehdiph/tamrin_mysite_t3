7108b762bf55858e513f45c8236cfdb8c74c8d1b
mahsa <mahsabiglary75@gmail.com>
2025-09-22 02:39:45 +0330

    Add about page and template structure; fix URLs and context processor

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -54,13 +55,14 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pages.context_processors.site_name',
             ],
         },
     },
---
+++ pages/context_processors.py
@@ -0,0 +1,4 @@
+def site_name(request):
+    return {
+        "site_name": "My Django Blog"
+    }
